770294f92d9933244acae2ce24c6cdaff38e55d4
Ali.Reza <alirzahosinopoor.1390@gmail.com>
2025-06-04 16:55:08 +0330

    fix: add mode to astro node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,9 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://fuwari.vercel.app/",
-	adapter: node(),
+	adapter: node({
+		mode: "standalone",
+	}),
 	base: "/",
 	trailingSlash: "always",
 
