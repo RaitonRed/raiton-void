929ab2a43bb3387f6d8840326262d7add6705e66
RaitonRed <alirzahosinopoor.1390@gmail.com>
2025-10-09 04:18:18 -0700

    added donate page to navbar via data

--- src/config.ts
+++ src/config.ts
@@ -43,11 +43,15 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		LinkPreset.Donate,
 		{
 			name: "GitHub",
 			url: "https://github.com/RaitonRed", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
+		},
+		{
+			name: "Donate",
+			url: "/donate",
+			external: false,
 		},
 	],
 };
